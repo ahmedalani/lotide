f61abb9fc45f0e32a06065d1d429953556115ce3
Ahmed Alani <s3leha@gmail.com>
2020-06-02 13:23:13 -0700

    added without.js, cleaned up eqArrays.js

--- without.js
+++ without.js
@@ -19,8 +19,3 @@ const assertEqual = function (actual, expected) {
   }
   console.log(`This ${actual}❗️does not match ${expected}`);
 };
-// tests
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
