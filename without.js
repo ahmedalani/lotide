3d9cd73075667f7e85c32c1cca65a45c5c508cfe
Ahmed Alani <s3leha@gmail.com>
2020-06-02 13:44:43 -0700

    without function works, added some tests as well

--- without.js
+++ without.js
@@ -1,21 +1,40 @@
 /* eslint-disable func-names, no-console */
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    const element = arr1[i];
-    if (element !== arr2[i]) {
-      return false;
+const words = ['hello', 'world', 'lighthouse'];
+const without = function (source, itemsToRemove) {
+  const output = [...source];
+  for (let i = 0; i < source.length; i++) {
+    const element = source[i];
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      const elementToRemove = itemsToRemove[j];
+      if (element === elementToRemove) {
+        output.splice(i, 1);
+      }
     }
   }
-  return true;
+  return output;
 };
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log('They are ✅ matching');
-    return;
-  }
-  console.log(`This ${actual}❗️does not match ${expected}`);
-};
+// tests
+console.log(without(['1', 2, 3], [1]));
+console.log(without(['1', '2', '3'], [1, 2, '3']));
+console.log(without(words, ['lighthouse']));
+console.log(words);
+
+// const eqArrays = function (arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     const element = arr1[i];
+//     if (element !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log('They are ✅ matching');
+//     return;
+//   }
+//   console.log(`This ${actual}❗️does not match ${expected}`);
+// };
