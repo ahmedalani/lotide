b6255736795c1b121d590be83ae918466e30c032
Ahmed Alani <s3leha@gmail.com>
2020-06-03 11:16:00 -0700

    countLetters function works, passed all tests

--- countLetters.js
+++ countLetters.js
@@ -7,3 +7,20 @@ const assertEqual = function (actual, expected) {
   }
   console.log(`This ${actual}❗️does not match ${expected}`);
 };
+// takes a string as parameter and return an obj with count of each letter
+const countLetters = function (str) {
+  const result = {};
+  for (const letter of str) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+// Tests
+const test = countLetters('lighthouse in the house');
+assertEqual(test['h'], 4);
+assertEqual(test['e'], 3);
+assertEqual(test.e, 3);
