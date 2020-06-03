7c522728ad2cd71223f517dd139f85e5b635eef5
Ahmed Alani <s3leha@gmail.com>
2020-06-03 11:48:31 -0700

    letterPositions function works, passed all tests

--- letterPositions.js
+++ letterPositions.js
@@ -20,3 +20,21 @@ const assertEqual = function (actual, expected) {
   }
   console.log(`This ${actual}❗️does not match ${expected}`);
 };
+// takes a sentence as parameter and return an and obj with each letter index (zero based)
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter === ' ') {
+      console.log('found space!');
+    } else if (results[letter]) {
+      results[letter].push(i);
+    } else {
+      results[letter] = [i];
+    }
+  }
+  return results;
+};
+// Tests
+const test = letterPositions('hello');
+assertEqual(eqArrays(test.l, [2, 3]), true);
