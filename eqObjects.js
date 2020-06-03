3c84186ed682e8da20d28ed855daa8ff9b8b19f5
Ahmed Alani <s3leha@gmail.com>
2020-06-03 13:32:03 -0700

    eqObjects function works with array as values

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-/* eslint-disable func-names, no-console, eol-last, no-restricted-syntax */
+/* eslint-disable func-names, no-console, eol-last, no-restricted-syntax, guard-for-in */
 // takes 2 params and check if they match
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
@@ -6,6 +6,19 @@ const assertEqual = function (actual, expected) {
     return;
   }
   console.log(`This ${actual}❗️does not match ${expected}`);
+};
+// takes two arrays an check if they equate
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    const element = arr1[i];
+    if (element !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 // takes 2 objects as params and return true or fals based on perfect match
 const eqObjects = function (obj1, obj2) {
@@ -19,6 +32,10 @@ const eqObjects = function (obj1, obj2) {
     smallerObj = { ...obj1 };
   }
   for (const key in biggerObj) {
+    if (Array.isArray(smallerObj[key])) {
+      const result = eqArrays(smallerObj[key], biggerObj[key]);
+      return result;
+    }
     if (!smallerObj[key] || smallerObj[key] !== biggerObj[key]) {
       return false;
     }
@@ -33,4 +50,10 @@ const z = { a: 1, b: '3' };
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
 assertEqual(eqObjects(ab, z), false);
+// tests with arrays
+const cd = { c: '1', d: ['2', 3] };
+const dc = { d: ['2', 3], c: '1' };
+assertEqual(eqObjects(cd, dc), true);
+const cd2 = { c: '1', d: ['2', 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
 
