1b146941676439c100efba9a95416f3b2d626572
Ahmed Alani <s3leha@gmail.com>
2020-06-03 10:33:35 -0700

    added countOnly.js file, function still work in progress

---
+++ countOnly.js
@@ -0,0 +1,34 @@
+/* eslint-disable func-names, no-console, eol-last */
+// takes 2 params and check if they match
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log('They are ✅ matching');
+    return;
+  }
+  console.log(`This ${actual}❗️does not match ${expected}`);
+};
+// takes arry and opject as params, count array elements based on classification from the obj
+const countOnly = function (allItems, itemsToCount) {
+
+};
+
+// tests
+/*
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+*/
