91856af189a080c85efb3bf058300153ef18bc3e
Ahmed Alani <s3leha@gmail.com>
2020-06-03 12:08:18 -0700

    findKeyByValue function is complete, passed tests

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,4 @@
-/* eslint-disable func-names, no-console, eol-last */
+/* eslint-disable func-names, no-console, eol-last, no-restricted-syntax */
 // takes 2 params and check if they match
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
@@ -8,8 +8,13 @@ const assertEqual = function (actual, expected) {
   console.log(`This ${actual}❗️does not match ${expected}`);
 };
 // takes an obj and a value as params, returns the first key that contain the matching value
-const findKeyByValue = function () {
-
+const findKeyByValue = function (obj, val) {
+  for (const key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  return undefined;
 };
 // Tests
 const bestTVShowsByGenre = {
@@ -17,6 +22,5 @@ const bestTVShowsByGenre = {
   comedy: 'Brooklyn Nine-Nine',
   drama: 'The Wire'
 };
-
 assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
 assertEqual(findKeyByValue(bestTVShowsByGenre, 'That \'70s Show'), undefined);
