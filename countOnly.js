39c40d57e9461d9204212ac5c039d2bf77afb7e7
Ahmed Alani <s3leha@gmail.com>
2020-06-03 10:58:16 -0700

    countOnly function works, all tests passed

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,4 @@
-/* eslint-disable func-names, no-console, eol-last */
+/* eslint-disable func-names, no-console, eol-last, dot-notation, no-restricted-syntax */
 // takes 2 params and check if they match
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
@@ -9,26 +9,33 @@ const assertEqual = function (actual, expected) {
 };
 // takes arry and opject as params, count array elements based on classification from the obj
 const countOnly = function (allItems, itemsToCount) {
-
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
 };
-
 // tests
-/*
 const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
+  'Karl',
+  'Salima',
+  'Agouhanna',
+  'Fang',
+  'Kavith',
+  'Jason',
+  'Salima',
+  'Fang',
+  'Joe'
 ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+const result1 = countOnly(firstNames, { Jason: true, Karima: true, Fang: true });
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-*/
+assertEqual(result1['Jason'], 1);
+assertEqual(result1['Karima'], undefined);
+assertEqual(result1['Fang'], 2);
