5398ab71be3627b664b49d0bc06b862366f0b57b
Ahmed Alani <s3leha@gmail.com>
2020-06-07 09:27:54 -0700

    used mocha and chai for testing middle, tail. all passing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,24 @@
-const assertArraysEqual = require('../assertArraysEqual');
+/* eslint-disable */
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle(['j']), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(['j', 'c', 'j']), ['c']);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe('middle', () => {
+  it('should return [] for middle of one element array', () => {
+    assert.deepEqual(middle(['j']), []);
+  });
+  it('should return [] for 2 element array', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("should return ['c'] for ['j', 'c', 'j']", () => {
+    assert.deepEqual(middle(['j', 'c', 'j']), ['c']);
+  });
+  it("should return [1, 2, 3, 4, 5] for [3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("should return [1, 2, 3, 4] for [2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("should return [1, 2, 3, 4, 5, 6] for [3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,16 @@
+/* eslint-disable */
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-const words = ['first', 'second', 'third'];
-tail(words);
-assertEqual(words.length, 3);
-assertEqual(tail([1, 2, 3, 4]).length, 3);
-assertEqual(tail([1]).length, 0);
+describe('tail', () => {
+  it("should return tail of ['first', 'second', 'third']", () => {
+    const words = ['first', 'second', 'third'];
+    assert.deepEqual(tail(words), ['second', 'third']);
+  });
+  it("should return tail of [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it("should return [] for tail of [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
