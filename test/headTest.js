2f4024e47a895e69ad5a686c7a62638b93d03c6e
Ahmed Alani <s3leha@gmail.com>
2020-06-07 09:02:32 -0700

    added .gitignore, package.json, modified test to use mocha and chai testing, all tests passed

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,21 @@
-const assertEqual = require('../assertEqual');
+/* eslint-disable */
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([]), undefined);
-assertEqual(head([1, 2, 3]), 1);
-assertEqual(head(['hello', 2, 3]), 'hello');
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns '1' for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 'hello' for ['hello', 2, 3]", () => {
+    assert.strictEqual(head(['hello', 2, 3]), 'hello');
+  });
+});
